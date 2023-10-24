92df87d72cf5ca35d814b4422ca766403e525959
LkMadMan <125731326+lkmadman@users.noreply.github.com>
2023-10-24 20:07:08 +0530

    images

---
+++ login_script.js
@@ -0,0 +1,129 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCksk0f0BWISsJO3goGj7kEnhX9vy8UZ_Q",
+  authDomain: "vinotest-5be90.firebaseapp.com",
+  databaseURL: "https://vinotest-5be90-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "vinotest-5be90",
+  storageBucket: "vinotest-5be90.appspot.com",
+  messagingSenderId: "150238579860",
+  appId: "1:150238579860:web:dc026fb28d9f289d8b7881",
+  measurementId: "G-MPLWX66VPS"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const databse = firebase.database();
+const submitButton = document.getElementById("submit");
+const signupButton = document.getElementById("sign-up");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const main = document.getElementById("main");
+const createacct = document.getElementById("create-acct");
+const signupEmailIn = document.getElementById("email-signup");
+const usernameIn = document.getElementById("username-signup");
+const signupPasswordIn = document.getElementById("password-signup");
+const confirmSignUpPasswordIn = document.getElementById(
+  "confirm-password-signup"
+);
+const createacctbtn = document.getElementById("create-acct-btn");
+const forgetBtn = document.querySelector(".forget-btn");
+
+// const returnBtn = document.getElementById("return-btn");
+
+var email,
+  password,
+  signupEmail,
+  signupPassword,
+  confirmSignUpPassword,
+  username,
+  userId;
+
+createacctbtn.addEventListener("click", function () {
+  var isVerified = true;
+  signupEmail = signupEmailIn.value;
+
+  signupPassword = signupPasswordIn.value;
+  confirmSignUpPassword = confirmSignUpPasswordIn.value;
+  if (signupPassword != confirmSignUpPassword) {
+    window.alert("Password fields do not match. Try again.");
+    isVerified = false;
+  }
+
+  username = usernameIn.value;
+  if (
+    signupEmail == null ||
+    signupPassword == null ||
+    confirmSignUpPassword == null
+  ) {
+    window.alert("Please fill out all required fields.");
+    isVerified = false;
+  }
+
+  if (isVerified) {
+    auth
+      .createUserWithEmailAndPassword(signupEmail, signupPassword)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+        console.log("Success! Account created.");
+        sessionStorage.setItem("userId", auth.currentUser.uid);
+        // console.log(auth.currentUser.uid);
+        databse
+          .ref("/Users/" + auth.currentUser.uid)
+          .set({
+            Basic_info: {
+              email_id: signupEmail,
+              user_name: username,
+            },
+          })
+          .then((res) => {
+            console.log("Data added to database.");
+            location.replace("./start.html");
+          })
+          .catch((error) => console.log(error));
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+        console.log(errorMessage);
+        window.alert("Error occurred. ErrorCode ", errorCode);
+      });
+  }
+});
+
+submitButton.addEventListener("click", function () {
+  email = emailInput.value;
+  password = passwordInput.value;
+  auth
+    .signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      sessionStorage.setItem("userId", auth.currentUser.uid);
+      console.log("Success! Welcome back!");
+      // window.alert("Success! Welcome back!");
+      location.replace("./start.html");
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage);
+      window.alert("Error occurred. ErrorCode ", errorCode);
+    });
+});
+
+// reseting password
+forgetBtn.addEventListener("click", function () {
+  email = emailInput.value;
+  auth
+    .sendPasswordResetEmail(email)
+    .then(() => {
+      window.alert("Password reset email sent, check your inbox. ");
+    })
+    .catch((error) => {
+      window.alert(error);
+    });
+});
---
+++ question.js
@@ -0,0 +1,60 @@
+let questions = [
+  {
+    numb: 1,
+    question: "Which of the following is the largest organ in the human body?",
+    answer: "Skin",
+    options: [
+      "Heart",
+      "Brain",
+      "Liver",
+      "Skin",
+    ],
+  },
+  {
+    numb: 2,
+    question: "How many bones are there in the adult human body?",
+    answer: "206",
+    options: ["150", "206", "250", "300"],
+  },
+  {
+    numb: 3,
+    question: "Which part of the human brain is responsible for regulating basic life functions such as breathing and heart rate?",
+    answer: "Brainstem",
+    options: [
+      "Cerebellum",
+      "Frontal Lobe",
+      "Brainstem",
+      "Hippocampus",
+    ],
+  },
+  {
+    numb: 4,
+    question: "What is the scientific name for the thigh bone?",
+    answer: "Femur",
+    options: ["Tibia", "Femur", "Radius", "Humerus"],
+  },
+  {
+    numb: 5,
+    question: "Which blood vessels carry oxygenated blood away from the heart?",
+    answer: "Arteries",
+    options: ["Veins", "Arteries", "Capillaries", "Venules"],
+  },
+  {
+    numb: 6,
+    question: "What is the name of the muscle that separates the chest from the abdomen and is essential for breathing?",
+    answer: "Diaphragm",
+    options: ["Biceps", "Quadriceps", "Diaphragm", "Hamstring"],
+  },
+  {
+    numb: 7,
+    question: "Which organ is responsible for filtering and detoxifying the blood in the human body?",
+    answer: "Liver",
+    options: ["Kidney", "Liver", "Spleen", "Lung"],
+  },
+  {
+    numb: 8,
+    question: "What is the name of the fluid that lubricates joints and reduces friction between bones?",
+    answer: "Synovial fluid",
+    options: ["Blood plasma", "Cerebrospinal fluid", "Lymph", "Synovial fluid"],
+  },
+];
